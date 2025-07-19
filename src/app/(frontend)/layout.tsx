bb47fda7d98bc7395e0dc6dd955d59891605827a
Shayan <shayanakhlaghian@gmail.com>
2025-07-19 16:32:10 +0330

    first commit

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,13 @@
+import './globals.css';
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ src/collections/index.ts
@@ -0,0 +1,4 @@
+import { Users } from './Users';
+import { Media } from './Media';
+
+export const collections = [Users, Media];
