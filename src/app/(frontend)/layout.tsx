b921f26636793d49d7de717de02783117404db17
Shayan <shayanakhlaghian@gmail.com>
2025-07-21 14:32:32 +0330

    add geist and geist mono as primary and accent fonts

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,11 +1,18 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+
 import './globals.css';
 import { Header } from '@/components';
+import { cn } from '@/lib';
 import Providers from './providers';
+
+const geist = Geist({ subsets: ['latin'], variable: '--font-geist' });
+
+const geistMono = Geist_Mono({ subsets: ['latin'], variable: '--font-geist-mono' });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className={cn(geist.variable, geistMono.variable, 'font-primary')}>
         <Providers>
           <Header />
           <main className="w-full overflow-x-hidden">{children}</main>
