a5fc81144f02f81c169f64e254c459c97bd07a33
Shayan <shayanakhlaghian@gmail.com>
2025-07-23 17:28:55 +0330

    complete hero model background

--- src/app/(frontend)/_components/Hero.tsx
+++ src/app/(frontend)/_components/Hero.tsx
@@ -8,7 +8,9 @@ const Hero = (hero: Landing['hero']) => {
     <section className="relative flex h-[calc(100vh-var(--header-height))] w-full flex-col lg:flex-row">
       <DotPattern className="absolute top-0 left-0 -z-10 [mask-image:radial-gradient(200px_circle_at_center,white,transparent)] lg:[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]" />
       <div className="flex flex-1 items-center justify-center">
-        <div className="size-40 rotate-45 rounded-3xl bg-primary md:size-44 lg:size-64 xl:size-80"></div>
+        <div className="size-40 rotate-45 rounded-3xl bg-secondary md:size-44 lg:size-64 xl:size-80">
+          <div className='size-full bg-primary rounded-3xl rotate-12'/>
+        </div>
       </div>
       <Greet {...hero} />
     </section>
