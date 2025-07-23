7670d9fdaf0788920ac6a066b1559e096e9edaa2
Shayan <shayanakhlaghian@gmail.com>
2025-07-23 17:25:37 +0330

    animate hero greet

---
+++ src/app/(frontend)/_components/Greet.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { useGSAP } from '@gsap/react';
+import { gsap, SplitText } from 'gsap/all';
+
+import type { Icon, Landing } from '@/payload-types';
+import { AppLink, MediaIcon, RainbowButton, RichText } from '@/components';
+
+const Greet = ({ title, description, cta }: Landing['hero']) => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create('.hero-title', { type: 'chars' });
+    const descriptionSplit = SplitText.create('.hero-description', { type: 'lines, words' });
+
+    gsap.fromTo(
+      titleSplit.chars,
+      { opacity: 0, yPercent: 100 },
+      { opacity: 1, yPercent: 0, duration: 1.2, ease: 'expo.out', stagger: 0.05 },
+    );
+
+    gsap.fromTo(
+      descriptionSplit.lines,
+      { opacity: 0, yPercent: 100 },
+      { opacity: 1, yPercent: 0, duration: 1.2, delay: 0.5, ease: 'expo.out', stagger: 0.05 },
+    );
+
+    gsap.fromTo(
+      '.hero-cta',
+      { opacity: 0, yPercent: 100 },
+      { opacity: 1, yPercent: 0, duration: 0.6, delay: 1, ease: 'expo.out' },
+    );
+  }, []);
+
+  return (
+    <div className="flex flex-1 flex-col gap-4 px-8 lg:justify-center lg:px-16">
+      <h1 className="hero-title bg-gradient-to-r from-primary to-secondary bg-clip-text font-accent text-2xl font-bold text-transparent lg:text-3xl">
+        {title}
+      </h1>
+      <RichText data={description} className="hero-description prose-p:my-0" />
+      <AppLink href={cta.href} newTab={cta.newTab} className="hero-cta">
+        <RainbowButton className="self-start capitalize">
+          {cta.text} <MediaIcon icon={cta.icon as Icon} />
+        </RainbowButton>
+      </AppLink>
+    </div>
+  );
+};
+
+export default Greet;
--- src/app/(frontend)/_components/Hero.tsx
+++ src/app/(frontend)/_components/Hero.tsx
@@ -1,24 +1,16 @@
-import type { Icon, Landing } from '@/payload-types';
-import { AppLink, DotPattern, MediaIcon, RainbowButton, RichText } from '@/components';
+'use client';
+import { Greet } from '.';
+import type { Landing } from '@/payload-types';
+import { DotPattern } from '@/components';
 
-const Hero = ({ title, description, cta }: Landing['hero']) => {
+const Hero = (hero: Landing['hero']) => {
   return (
     <section className="relative flex h-[calc(100vh-var(--header-height))] w-full flex-col lg:flex-row">
       <DotPattern className="absolute top-0 left-0 -z-10 [mask-image:radial-gradient(200px_circle_at_center,white,transparent)] lg:[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]" />
       <div className="flex flex-1 items-center justify-center">
         <div className="size-40 rotate-45 rounded-3xl bg-primary md:size-44 lg:size-64 xl:size-80"></div>
       </div>
-      <div className="flex flex-1 flex-col gap-4 px-8 lg:justify-center lg:px-16">
-        <h1 className="bg-gradient-to-r from-primary to-secondary bg-clip-text font-accent text-2xl font-bold text-transparent lg:text-3xl">
-          {title}
-        </h1>
-        <RichText data={description} />
-        <AppLink href={cta.href} newTab={cta.newTab}>
-          <RainbowButton className="self-start capitalize">
-            {cta.text} <MediaIcon icon={cta.icon as Icon} />
-          </RainbowButton>
-        </AppLink>
-      </div>
+      <Greet {...hero} />
     </section>
   );
 };
