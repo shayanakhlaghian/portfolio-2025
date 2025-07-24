ec9d936e1f603bfb74a002894f3eb2ce2b16b917
Shayan <shayanakhlaghian@gmail.com>
2025-07-24 17:09:17 +0330

    add type-animation to the crt screen

--- src/models/Crt.tsx
+++ src/models/Crt.tsx
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, Html, Plane } from '@react-three/drei';
 import { GLTF } from 'three-stdlib';
+import { TypeAnimation } from 'react-type-animation';
 
+import { typeAnimationSequence } from '@/constants';
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -25,8 +27,28 @@ const Crt = (props: React.JSX.IntrinsicElements['group']) => {
     <group {...props} dispose={null}>
       <mesh geometry={nodes['Node-Mesh'].geometry} material={materials.mat15} />
       <mesh geometry={nodes['Node-Mesh_1'].geometry} material={materials.mat17} />
-      <mesh geometry={nodes['Node-Mesh_2'].geometry} material={materials.mat16} />
       <mesh geometry={nodes['Node-Mesh_3'].geometry} material={materials.mat22} />
+      {/* <mesh geometry={nodes['Node-Mesh_2'].geometry} material={materials.mat16} /> */}
+      <Plane
+        args={[0.47, 0.417]}
+        rotation-y={Math.PI}
+        position={[0.015, 0.2125, -0.34]}
+        material-color="black"
+      >
+        <Html
+          transform
+          occlude="blending"
+          position={[0, 0, 0.001]}
+          distanceFactor={1}
+          className="h-[170px] w-[190px] bg-black"
+        >
+          <TypeAnimation
+            sequence={typeAnimationSequence}
+            repeat={Infinity}
+            className='text-[10px] text-white'
+          />
+        </Html>
+      </Plane>
     </group>
   );
 };
