d3261eb86b6d8e0cac35b41509b076a2cc092265
Shayan <shayanakhlaghian@gmail.com>
2025-07-31 15:29:19 +0330

    send email on message received

--- src/actions/contact.ts
+++ src/actions/contact.ts
@@ -1,4 +1,6 @@
 'use server';
+import nodemailer from 'nodemailer';
+
 import type { Message } from '@/payload-types';
 import { getPayload } from '.';
 import { catchAction, contactSchema } from '@/lib';
@@ -7,10 +9,26 @@ export const sendMessageAction = catchAction(async (data) => {
   const _data = data as Message;
 
   const payload = await getPayload();
-  payload.create({
+  await payload.create({
     collection: 'messages',
     data: _data,
   });
+
+  nodemailer
+    .createTransport({
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT),
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    })
+    .sendMail({
+      from: `<${process.env.EMAIL_USER}>`,
+      to: _data.email,
+      subject: 'Thanks for your message!',
+      text: 'Thank you for your message! I will get back to you as soon as I can.',
+    });
 
   return { success: true };
 }, contactSchema);
